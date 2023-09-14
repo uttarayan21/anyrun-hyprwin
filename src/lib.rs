686c3cece7e4bc87d130b4b4445e3d2a0cca30ec
Uttarayan <uttarayan21@gmail.com>
2023-09-14 22:11:55 +0530

    [fix] Do not fail to focus seperate windows having same pid

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,42 @@
+use core::ops::Deref;
+
 use abi_stable::std_types::{ROption, RString, RVec};
 use anyrun_plugin::*;
-use hyprland::data::Clients;
+use hyprland::data::{Client, Clients};
 use hyprland::shared::HyprData;
 
+#[derive(Debug, Clone)]
+pub struct ClientId {
+    pub client: Client,
+    pub search: String,
+    pub id: u64,
+}
+
+pub struct State {
+    pub clients: Vec<ClientId>,
+}
+
+impl Deref for ClientId {
+    type Target = Client;
+    fn deref(&self) -> &Self::Target {
+        &self.client
+    }
+}
+
 #[init]
-fn init(_: RString) -> Clients {
-    Clients::get().expect("Failed to get clients")
+fn init(_: RString) -> State {
+    State {
+        clients: Clients::get()
+            .expect("Failed to get clients")
+            .filter(|client| !(client.title.is_empty() && client.class.is_empty()))
+            .enumerate()
+            .map(|(idx, client)| ClientId {
+                id: idx as u64,
+                search: format!("{}: {}", client.class, client.title),
+                client,
+            })
+            .collect(),
+    }
 }
 
 #[info]
@@ -17,43 +48,46 @@ fn info() -> PluginInfo {
 }
 
 #[get_matches]
-fn get_matches(input: RString, clients: &Clients) -> RVec<Match> {
+fn get_matches(input: RString, state: &State) -> RVec<Match> {
     use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
     let matcher = SkimMatcherV2::default();
-    clients
-        .clone()
+    state
+        .clients
+        .iter()
         .filter(|c| !(c.title.is_empty() && c.class.is_empty()))
-        .map(|mut client| {
-            client.title = format!("{}: {}", client.class, client.title);
-            client
-        })
-        .filter(|client| matcher.fuzzy_match(&client.title, &input).is_some())
-        .map(|client| {
-            Match {
-                title: client.class.clone().into(),
-                icon: ROption::RSome(icon_from_class(client.class).into()),
-                use_pango: false,
-                description: ROption::RSome(client.title.into()),
-                id: ROption::RSome(client.pid as u64), // The ID can be used for identifying the match later, is not required
-            }
+        .filter(|client| matcher.fuzzy_match(&client.search, &input).is_some())
+        .map(|client| Match {
+            title: client.class.clone().into(),
+            icon: ROption::RSome(icon_from_class(&client.class).into()),
+            use_pango: false,
+            description: ROption::RSome(client.title.clone().into()),
+            id: ROption::RSome(client.id),
         })
         .collect::<Vec<Match>>()
         .into()
 }
 
 #[handler]
-fn handler(selection: Match) -> HandleResult {
+fn handler(selection: Match, state: &State) -> HandleResult {
     // Handle the selected match and return how anyrun should proceed
     use hyprland::dispatch::*;
-    Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::ProcessId(
-        selection.id.unwrap_or_default() as u32,
+    let Some(address) = state
+        .clients
+        .iter()
+        .find(|c| c.id == selection.id.unwrap_or_default())
+        .map(|c| c.address.clone())
+    else {
+        return HandleResult::Close;
+    };
+    Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::Address(
+        address,
     )))
     .expect("Unable to focus hyprland window");
     HandleResult::Close
 }
 
-fn icon_from_class(class: String) -> String {
-    let class = class.to_lowercase();
+fn icon_from_class(class: impl AsRef<str>) -> String {
+    let class = class.as_ref().to_lowercase();
     if class.contains('.') {
         class.split('.').last().unwrap_or_default().into()
     } else {
